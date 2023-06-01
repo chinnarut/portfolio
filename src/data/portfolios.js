4397de7cfd997ab6313f89caf6f2d46d053c2229
Chinnarut Tamprateep <chinnarut.t@gmail.com>
2023-06-01 08:38:49 +0700

    add project

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -3,7 +3,7 @@ import { eye, git } from "../utils/Icons";
 import gpt3 from '../img/portImages/gpt3.png';
 import music from '../img/portImages/music.png';
 import airdnd from "../img/portImages/airdnd.png";
-import chatapp from "../img/portImages/chat-app.png";
+import carbox from "../img/portImages/carbox.png";
 import cryptoQ from "../img/portImages/cryptoQ.png";
 import portfolio from "../img/portImages/portfolio.png";
 
@@ -14,17 +14,7 @@ export const portfolios = [
         title: "Airdnd Booking App",
         image: airdnd,
         link1: "https://github.com/chinnarut/airdnd-booking-app",
-        link2: "https://airdnd-booking-app.vercel.app/",
-        icon1: git,
-        icon2: eye,
-    },
-    {
-        id: uuid(),
-        category: "Full Stack Web Application",
-        title: "MERN Chat App",
-        image: chatapp,
-        link1: "https://github.com/chinnarut/chat-app",
-        link2: "https://fullstack-chat-app-chinnarut.vercel.app",
+        link2: "https://airdnd-booking-app.vercel.app",
         icon1: git,
         icon2: eye,
     },
@@ -44,7 +34,7 @@ export const portfolios = [
         title: "CryptoQ",
         image: cryptoQ,
         link1: "https://github.com/chinnarut/cryptoQ",
-        link2: "https://crypto-q.vercel.app/",
+        link2: "https://crypto-q.vercel.app",
         icon1: git,
         icon2: eye,
     },
@@ -54,7 +44,7 @@ export const portfolios = [
         title: "My Portfolio",
         image: portfolio,
         link1: "https://github.com/chinnarut/portfolio",
-        link2: "https://chinnarut-porfoliot-web.vercel.app/",
+        link2: "https://chinnarut-porfoliot-web.vercel.app",
         icon1: git,
         icon2: eye,
     },
@@ -65,6 +55,16 @@ export const portfolios = [
         image: gpt3,
         link1: "https://github.com/chinnarut/gpt-ui-design",
         link2: "https://gpt-ui-design.netlify.app",
+        icon1: git,
+        icon2: eye,
+    },
+    {
+        id: uuid(),
+        category: "Full Stack Web Application",
+        title: "CarDataBox",
+        image: carbox,
+        link1: "https://github.com/chinnarut/car-data-box",
+        link2: "https://car-data-box.vercel.app",
         icon1: git,
         icon2: eye,
     }
